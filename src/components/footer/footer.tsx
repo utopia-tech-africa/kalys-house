3cda311995c4c95135add6fd6c964386c8c8dd6f
Linda Mensah <lindaakmensah@gmail.com>
2025-11-27 15:57:19 +0000

    start registration section

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,13 +3,13 @@ import { Copyright } from "./copyright";
 import { Join } from "./join";
 import { SiteMap } from "./site-map";
 import { Socials } from "./socials";
-import { FooterImg } from "@/app/assets/img";
+import { BackDropImg } from "@/app/assets/img";
 
 const Footer = () => {
   return (
     <footer className="relative w-full overflow-hidden">
       <Image
-        src={FooterImg}
+        src={BackDropImg}
         alt="footer image"
         fill
         className="absolute inset-0 object-cover -z-20"
