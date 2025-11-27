f1c06778b376fdc9752d998c7a2787cd82546948
efbrace <ebenezerflintwoodbrace@gmail.com>
2025-11-27 17:01:37 +0000

    Complete schedule component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { EventBrief } from "./components/event-brief/event-brief";
 import { Hero } from "./components/hero/hero";
+import { Schedule } from "./components/schedule/schedule";
 import { Sponsors } from "./components/sponsors/sponsors";
 import { WhatDeyGoOn } from "./components/what-dey-go-on/what-dey-go-on";
 import Registration from "@/components/registration/registration";
@@ -11,6 +12,7 @@ const Home = () => {
       <EventBrief />
       <Sponsors />
       <WhatDeyGoOn />
+      <Schedule />
     </>
   );
 };
