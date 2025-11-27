c8ea545c3e41ec7cf0df2090ce0982156237c29b
Linda Mensah <lindaakmensah@gmail.com>
2025-11-27 17:54:49 +0000

    implement registration section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
+import Registration from "@/components/registration/registration";
 import { EventBrief } from "./components/event-brief/event-brief";
 import { Hero } from "./components/hero/hero";
 import { Sponsors } from "./components/sponsors/sponsors";
 import { WhatDeyGoOn } from "./components/what-dey-go-on/what-dey-go-on";
-import Registration from "@/components/registration/registration";
 
 const Home = () => {
   return (
@@ -11,6 +11,7 @@ const Home = () => {
       <EventBrief />
       <Sponsors />
       <WhatDeyGoOn />
+      <Registration />
     </>
   );
 };
--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,7 +1,17 @@
 import logo from "./kalys-house-logo.webp";
 import cocacolaLogo from "./cocacola-logo.png";
+import backdropImg1 from "./backdrop-img1.webp";
+import backdropImage2 from "./backdrop-img-2.webp";
 import mtnLogo from "./mtn-logo.png";
 import sponsorCardBg from "./sponsor-card-bg.webp";
 import whatDeyGoOnBannerImg from "./what-dey-go-on-banner-img.webp";
 
-export { logo, cocacolaLogo, mtnLogo, sponsorCardBg, whatDeyGoOnBannerImg };
+export {
+  logo,
+  cocacolaLogo,
+  mtnLogo,
+  sponsorCardBg,
+  whatDeyGoOnBannerImg,
+  backdropImg1,
+  backdropImage2,
+};
