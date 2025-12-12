4da2ccd60c9b068ba82e62e70d9ffa116a761ea0
Linda Mensah <lindaakmensah@gmail.com>
2025-12-12 10:48:07 +0000

    add scroll mt to pitch section

--- src/components/collaborate/collaborate-button.tsx
+++ src/components/collaborate/collaborate-button.tsx
@@ -1,9 +1,12 @@
+import { Button } from "../ui/button";
+
 export const CollaborateButton = () => {
   return (
-    <div className="flex flex-col items-center justify-center rounded-lg bg-[linear-gradient(90deg,#FF3401_0%,#991F01_100%)] py-2 px-5 md:py-2 md:px-8 lg:py-3 lg:px-14">
-      <button className="text-base md:text-lg lg:text-[32px] text-nowrap cursor-pointer">
-        GET PLUGGED IN
-      </button>
-    </div>
+    // <div className="flex flex-col items-center justify-center rounded-lg bg-[linear-gradient(90deg,#FF3401_0%,#991F01_100%)] py-2 px-5 md:py-2 md:px-8 lg:py-3 lg:px-14 hover:bg-black/60 hover:border hover:border-[#FF3401]">
+
+    <Button className="text-base md:text-lg lg:text-[32px] text-nowrap cursor-pointer  rounded-lg bg-[linear-gradient(90deg,#FF3401_0%,#991F01_100%)] py-2 px-5 md:py-2 md:px-8 lg:py-3 lg:px-14 hover:bg-black/60 hover:border hover:border-[#FF3401]">
+      GET PLUGGED IN
+    </Button>
+    // </div>
   );
 };
